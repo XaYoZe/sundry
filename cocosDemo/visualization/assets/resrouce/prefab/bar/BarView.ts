8cff3ee08575ba055e06258c47b66cd9f4fbf61a
xayoze <yang6450867@gmail.com>
2024-09-26 19:07:40 +0800

    適配屏幕寬度

--- cocosDemo/visualization/assets/resrouce/prefab/bar/BarView.ts
+++ cocosDemo/visualization/assets/resrouce/prefab/bar/BarView.ts
@@ -1,4 +1,4 @@
-import { _decorator, Component, Node, instantiate, tween, math, Camera, Script } from 'cc';
+import { _decorator, Component, Node, instantiate, tween, math, Camera, NodeEventType, renderer, view } from 'cc';
 import { ViewCommon } from '../../../script/ViewCommon';
 import { BarItem } from './BarItem';
 const { ccclass, property } = _decorator;
@@ -11,16 +11,25 @@ export class BarView extends ViewCommon {
     lineNodeList: any[] = [];
     init (config: {size: number, width: number}) {
       let size = config.size ;
-      let width = config.width; 
-      for (let i = 0; i < size; i++) {
-        let nodeItem = instantiate(this.BarItemNode.node);
-        nodeItem.setScale(width, 1, 1)
-        nodeItem.setPosition((i * width) + (0.5 * width) , 0, 0);
-        this.node.addChild(nodeItem);
-        this.lineNodeList.push(nodeItem);
-        nodeItem.active = true;
-      }
-      this.inited = true;
+      let cameraNode = this.node.getChildByName('Camera')
+      let camera = cameraNode.getComponent(Camera)
+      this.scheduleOnce(() => {
+        /** 攝像機x起點 */
+        let {x: xStart} = camera.screenToWorld(new math.Vec3(0, 0, 0));
+        /** 攝像機x終點 */
+        let {x: xEnd } = camera.screenToWorld(new math.Vec3(camera.camera.width, 0, 0));
+        /** 計算寬度 */
+        let width  = (xEnd - xStart) / size;
+        for (let i = 0; i < size; i++) {
+          let nodeItem = instantiate(this.BarItemNode.node);
+          nodeItem.setScale(width, 1, 1)
+          nodeItem.setPosition(xStart + (i * width) + (0.5 * width) , 0, 0);
+          this.node.addChild(nodeItem);
+          this.lineNodeList.push(nodeItem);
+          nodeItem.active = true;
+        }
+        this.inited = true;
+      })
     }
     updateView (u8aCache: Uint8Array, deltaTime: number) {
       u8aCache.forEach((item, index) => {
